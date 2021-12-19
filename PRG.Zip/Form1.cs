c07d8ce44d1bd426e52191a90ee022f1d73806f1
Ivo van der Bruggen <vanderbruggenivo@gmail.com>
2021-12-19 22:16:57 +0100

    Added new functionality to Zip button

--- PRG.Zip/Form1.cs
+++ PRG.Zip/Form1.cs
@@ -8,6 +8,8 @@ namespace PRG.Zip
 {
     public partial class Form1 : Form
     {
+        private static int _addedBits = 0;
+        
         public Form1()
         {
             InitializeComponent();
@@ -15,8 +17,13 @@ namespace PRG.Zip
 
         private void BtnZip_Click(object sender, EventArgs e)
         {
-            StreamReader sr = FileHelper.OpenFile();
-            int[] frequency = Zipper.CalculateFrequency(sr);
+            OpenFileDialog dialog = FileHelper.OpenFile();
+            FolderBrowserDialog outputFolder = FileHelper.OpenFolder();
+            
+            string input = new StreamReader(dialog.FileName).ReadToEnd();
+            string fileExtension = Path.GetExtension(dialog.FileName);
+            
+            byte[] frequency = Zipper.CalculateFrequency(input);
             
             Node n;
             for (int i = 0; i < frequency.Length; i++)
@@ -24,6 +31,20 @@ namespace PRG.Zip
                     n = Dll.AddInOrder(frequency[i], Convert.ToByte(i));
 
             Tree.BuildTree();
+            Dll.BuildPath(Dll.Head);
+            
+            var encodedString = Tree.EncodeString(input, Dll.paths);
+            while (encodedString.Length % 8 != 0)
+            {
+                encodedString += '0';
+                _addedBits++;
+            }
+
+            var fileContents = $"{encodedString} {_addedBits} {fileExtension}";
+
+            var fileName = dialog.SafeFileName.Split('.')[0];
+            
+            FileHelper.CreateFile(outputFolder.SelectedPath, fileName, fileContents);
         }
     }
 }
