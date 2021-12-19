fc783b67a8d2207398edd793137068ae03ba3af7
Ivo van der Bruggen <vanderbruggenivo@gmail.com>
2021-12-19 22:15:45 +0100

    Refactored Node class to its own file

---
+++ PRG.Zip/Helpers/Node.cs
@@ -0,0 +1,22 @@
+﻿namespace PRG.Zip.Helpers
+{
+    public class Node
+    {
+        public Node Previous;
+        public Node Next;
+
+        public Node Left;
+        public Node Right;
+
+        public int Frequency;
+        public byte Value;
+
+        public Node(int frequency, byte value)
+        {
+            Frequency = frequency;
+            Value = value;
+        }
+        
+        public bool IsLeaf() => Left == null && Right == null;
+    }
+}
